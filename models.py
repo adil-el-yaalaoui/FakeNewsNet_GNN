bbb1cd714e9631131f98b78685a61294091ed934
Adil El Yaalaoui <adil@Adils-MacBook-Air.local>
2025-03-18 14:42:04 +0100

    add generic simple models

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ from sklearn.metrics import f1_score
 from torch_geometric.datasets import PPI
 from torch_geometric.loader import DataLoader
 
-from torch_geometric.nn import GATConv, global_max_pool
+from torch_geometric.nn import GATConv, global_max_pool, GCNConv, SAGEConv, SimpleConv
 
 
 class GATLayer(nn.Module):
@@ -163,6 +163,7 @@ class GNNClassifier(torch.nn.Module):
 
         self.lin = torch.nn.Linear(hidden_channels, 1)
         self.activation=nn.ReLU()
+        self.name="gat"
 
     def forward(self, x, edge_index,batch):
         x = self.conv1(x, edge_index)
@@ -178,10 +179,73 @@ class GATClassifier(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels):
         super().__init__()
         self.gat=GAT_Model(head=4,final_head=6,in_feature_size=in_channels,out_feature_size=hidden_channels,nb_classes=1)
+        self.name="gat_perso"
 
     def forward(self,x,edge_index,batch):
         x=self.gat(x,edge_index)
 
         x=global_max_pool(x,batch)
         return x.squeeze(1)
+
+
+class GenericModel(nn.Module):
+    def __init__(self, num_features,hidden_dim,num_classes,model,concat=False) -> None:
+        """
+        Generic class to test classic models for our dataset
+        """
+        super().__init__()
+        self.num_features=num_features
+        self.hidden_dim=hidden_dim
+        self.num_classes=num_classes
+        self.model=model
+        self.concat=concat
+        self.relu=nn.ReLU()
+        
+
+        if self.model=="gcn":
+            # Implement a graph convolution network
+            self.conv=GCNConv(self.num_features,self.hidden_dim)
+            self.name="gcn"
+        
+        elif self.model=="sage":
+            # Implement a sage graph convolutional network
+            self.conv=SAGEConv(self.num_features,self.hidden_dim)
+            self.name="sage"
+        
+        else:
+            # Default model which is a simple message passing with average neighbors nodes features
+            self.conv=SimpleConv(aggr="mean")
+            self.concat=False 
+            self.name="simple"
+
+        if self.concat:
+            # This is used to project the original input and concatenante it with th output of the model
+            # Can be seen as a type of skip connection
+            self.lin0 = nn.Linear(self.num_features, self.hidden_dim)
+            self.lin1 = nn.Linear(self.hidden_dim * 2, self.hidden_dim)
+        
+        self.lin2=nn.Linear(self.hidden_dim,self.num_classes)
+
+    
+    def forward(self,x,edge_index,batch):
+
+        output=self.relu(self.conv(x,edge_index))
+
+        output= global_max_pool (output,batch)
+
+        if self.concat:
+            # We need to create a tensor of shape (batch_size, num_nodes, features)
+            # to make it pass through the linear projection layer
+
+            number_of_graphs=torch.max(batch)+1
+            
+            skip=torch.stack([x[(batch==idx).nonzero().squeeze()[0]] for idx in range(number_of_graphs) ]) # tensor of shape (batch_size, features) because we took only one node
+
+            skip=self.relu(self.lin0(skip)) 
+
+            output=torch.cat([output,skip],dim=1)
+
+            output=self.relu(self.lin1(output))
+        
+        return self.lin2(output).squeeze(1)
 
